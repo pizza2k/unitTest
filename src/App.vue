0c706dec7f59db33e14070c626440205a37d207a
Zhenqi Wang <22373569@buaa.edu.cn>
2025-04-06 00:21:20 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <script setup>
 
+import HeadMenu from "@/components/head-menu.vue";
 </script>
 
 <template>
-  <router-view></router-view>
+    <div class="el-menu">
+        <headMenu :my_index="1">
+            <router-view></router-view>
+        </headMenu>
+    </div>
 </template>
 
 <style>
-#app{
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
+#app {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
 }
 </style>
--- src/views/main.vue
+++ src/views/main.vue
@@ -3,23 +3,10 @@ import headMenu from '../components/head-menu.vue';
 </script>
 
 <template>
-    <div id="my-main">
-        <div class="el-menu"><headMenu :my_index="1"></headMenu></div>
-    </div>
 </template>
 <style>
 html, body {
     height: 100%;
     margin: 0;
 }
-#my-main {
-    margin-top: 0vh;
-    width: 100%; /* 宽度铺满整个屏幕 */
-    height: 100%; /* 高度铺满整个屏幕 */
-    /* background-color: rgb(114, 196, 196); */
-}
-.el-menu {
-    margin-top: 3vh; 
-}
-
 </style>
